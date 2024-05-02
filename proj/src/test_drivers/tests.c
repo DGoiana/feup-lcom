4154bc1dc5360b71704689f461f081a1ccb37749
Alexandre Silva <up202206633@fe.up.pt>
2024-05-02 15:28:06 +0100

    remove useless files, add test functions

--- proj/src/test_drivers/tests.c
+++ proj/src/test_drivers/tests.c
@@ -110,3 +110,15 @@ int (test_mouse)() {
 
   return 0;
 }
+
+int (rtc_test_conf)(void) {
+	/* To be completed */
+}
+
+int (rtc_test_date)(void) {
+	/* To be completed */
+}
+
+int (rtc_test_int)(/* to be defined in class */) { 
+	/* To be completed */
+}
