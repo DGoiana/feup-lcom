27eff8b1d65d5b11ed9a0a26744be43cf6f9bad3
Alexandre Silva <up202206633@fe.up.pt>
2024-05-02 15:39:33 +0100

    work

--- proj/src/drivers/rtc/rtc.c
+++ proj/src/drivers/rtc/rtc.c
@@ -1,2 +1,13 @@
 #include "rtc.h"
+#include "i8254.h"
+
+int (rtc_read_reg)(uint8_t reg, uint8_t *conf) {
+	if (sys_outb(RTC_WRITE_DATA_REG, reg) != 0) {
+		printf("error writing to rtc register");
+		return 0;
+	}
+
+	if (util)
+
+}
 
